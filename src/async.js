ececb388911e03a6c21468a2f7f984c8015071ab
Efrain <efraincabrera35@gmail.com>
2022-09-22 08:47:06 -0400

    dia 1 de practica

--- src/async.js
+++ src/async.js
@@ -1,2 +1,22 @@
-let num = 5;
-console.log(num);
+function sum (a , b) {
+    return a + b;
+}
+
+function calc(a , b , sum){
+    return sum(a,b);
+}
+
+console.log(calc(2,5,sum));
+
+
+setTimeout(()=>{
+    console.log("hola mundo");
+},1000);
+
+
+const myName = (name) => {
+    console.log(`hola mi nombre es ${name}`);
+}
+
+setTimeout(myName,5000,"oscar");
+
